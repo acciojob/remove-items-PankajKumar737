6981a53e2fa3c3be4038996c2cee3b472ac06296
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-26 11:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let color = document.getElementById("colorSelect");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+	color.remove(color.selectedIndex);
+});
